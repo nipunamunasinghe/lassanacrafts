190fadaf0f0f1c317ce82534dfdd7ed1620b93dd
nipunamunasinghe <nipunamunasinghe1029@gmail.com>
2022-12-30 23:59:02 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
 import './App.css';
+import Logo from './images/Logo.png';
 
 function App() {
   return (
-    <div class="container"> 
-  <header>
-    <div class="logo">
-     {/* <img src="https://www.streamlineicons.com/ux/img/img-1.svg"/> */}
+    <div className="container">
+      <img className="image" src={Logo} alt="Logo"  />
       <h1>Lassana Crafts</h1>
+      <h2>Coming Soon</h2>
     </div>
-    <ul>
-      <li>About</li>
-      <li>Contact</li>
-    </ul>
-  </header>
-  <section>
-   {/* <img src="https://www.streamlineicons.com/ux/img/img-1.svg"/> */}
-    <p>coming soon</p>
-    <p>Are You Ready ?</p>
-    <form onsubmit="return false">
-      <input type="submit" value="Notify me"/>
-      <input type="email" placeholder="Your Email"/>
-    </form>
-    <div class="clear-fix"></div>
-   {/* <img src="https://www.streamlineicons.com/ux/img/style-8.svg"/> */}
-   {/* <img src="https://www.streamlineicons.com/ux/img/style-8.svg"/> */}
-  </section>
-</div>
   );
 }
 

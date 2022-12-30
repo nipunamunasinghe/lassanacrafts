039e324d2389dc18abcd8a1dc81e024d46373ba2
nipunamunasinghe <nipunamunasinghe1029@gmail.com>
2022-12-30 12:52:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container"> 
+  <header>
+    <div class="logo">
+     {/* <img src="https://www.streamlineicons.com/ux/img/img-1.svg"/> */}
+      <h1>Lassana Crafts</h1>
     </div>
+    <ul>
+      <li>About</li>
+      <li>Contact</li>
+    </ul>
+  </header>
+  <section>
+   {/* <img src="https://www.streamlineicons.com/ux/img/img-1.svg"/> */}
+    <p>coming soon</p>
+    <p>Are You Ready ?</p>
+    <form onsubmit="return false">
+      <input type="submit" value="Notify me"/>
+      <input type="email" placeholder="Your Email"/>
+    </form>
+    <div class="clear-fix"></div>
+   {/* <img src="https://www.streamlineicons.com/ux/img/style-8.svg"/> */}
+   {/* <img src="https://www.streamlineicons.com/ux/img/style-8.svg"/> */}
+  </section>
+</div>
   );
 }
 
